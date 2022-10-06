3635d3353556fc152b368f02b51f27d13d00baa9
Junghyun Josh Kim <111531443+junghyun-josh-kim@users.noreply.github.com>
2022-10-06 16:23:54 -0400

    JS: Canvas
    a basic person

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const context = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 800;
 
-context.fillRect(200, 200, 50, 200);
-context.fillRect(400, 200, 50, 200);
-context.lineWidth = 2;
-context.fillRect(300, 300, 50, 100);
-context.fillRect(200, 200, 200, 20);
+context.fillRect(210 - 40, 200 - 20, 15, 100);
+context.fillRect(350 - 40, 200 - 20, 15, 100);
+context.fillRect(260 - 40, 200 - 20, 60, 200);
+context.arc(250, 100, 50, 0, 2 * Math.PI);
+context.fill();
 
-context.moveTo(200, 200);
-context.lineTo(325, 100);
-context.lineTo(450, 200);
+context.beginPath();
+context.fillStyle = 'white';
+context.arc(260 + 10, 80, 8, 1 * Math.PI, 2 * Math.PI);
+context.arc(220 + 10, 80, 8, 1 * Math.PI, 2 * Math.PI);
 context.fill();
