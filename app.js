a540393d807331c178bcd3ab4c7c07c29d0324a2
Junghyun Josh Kim <111531443+junghyun-josh-kim@users.noreply.github.com>
2022-10-06 16:12:48 -0400

    JS: Canvas
    a basic house

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const context = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 800;
 
-context.moveTo(50, 50);
-context.lineTo(150, 50);
-context.lineTo(150, 150);
-context.lineTo(50, 150);
-context.lineTo(50, 50);
-context.stroke();
+context.fillRect(200, 200, 50, 200);
+context.fillRect(400, 200, 50, 200);
+context.lineWidth = 2;
+context.fillRect(300, 300, 50, 100);
+context.fillRect(200, 200, 200, 20);
+
+context.moveTo(200, 200);
+context.lineTo(325, 100);
+context.lineTo(450, 200);
+context.fill();
